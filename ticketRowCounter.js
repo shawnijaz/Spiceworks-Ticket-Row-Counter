e09e1223f8d1d67e9c60ba5aa6a48f59247dc2de
Shawn Ijaz <54557297+shawnijaz@users.noreply.github.com>
2022-06-16 16:41:46 -0500

    Update ticketRowCounter.js

--- ticketRowCounter.js
+++ ticketRowCounter.js
@@ -4,107 +4,79 @@
 // @version      0.1
 // @description  Counting the rows in each ticket container
 // @author       Shawn Ijaz
-// @match        https://on.spiceworks.com/tickets/*
+// @match        https://on.spiceworks.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // ==/UserScript==
 /* globals jQuery, $, waitForKeyElements */
 
-
-
 $( document ).ready(function() {
 
-    var createSpan = $(".dropdown-label.spec-dropdown-label").append( "<span id='numberVal'>  </span>" );
-    var numOfRows = $('.spec-ticket-item.ticket-item.ember-view:not(.loading)').length;
-    var updateNumOfRows;
-    $("#numberVal").text( ' (' + numOfRows + ')' );
+    var scroll = false
+    var clickCheck = false;
+    var createSpan = $(".dropdown-label.spec-dropdown-label").append( "<span id='numberVal'>  </span>" ); //adds span tag next to the dropdown label to display number of tickets
+    var numOfRows = $('.spec-ticket-item.ticket-item.ember-view:not(.loading)').length; //Counts how many tickets
+    $("#numberVal").text( ' (' + numOfRows + ')' ); //Displays number of tickets
 
-    $('.ticket-view-link.ember-view.sui-dropdown_entry').click(function() {
-        console.log('click event');
+    //Adding refresh button here for convenience
+    $('#status_dropdown').append(
+        $(document.createElement('button')).prop({
+            type: 'button',
+            innerHTML: 'Refresh',
+            class: 'btn btn-default refreshButton',
+        })
+    );
 
-        // $('.print-hide.ember-view.sw-loading-spinner.loading-spinner:not(.loading)')
-        // $('.print-hide.ember-view.sw-loading-spinner.loading-spinner.loading')
-
-        setTimeout(function() {
-            if($('.print-hide.ember-view.sw-loading-spinner.loading-spinner').hasClass('loading')) { console.log('loading'); }
-            else { console.log('Class loading not Found');
-
-                 }
-        }, 100);
-
-
-        /*         setTimeout(function() {
-            $('#ember3243').scrollTop($('#ember3243')[0].scrollHeight);
-        }, 3000); */
-
+    //Function to count the tickets after dropdown is selected
+    //There is a slight delay when the dropdown button is clicked. Because of this, a timer function had to be use so that we can display results once page is loaded
+    $.fn.TicketCounter = function()
+    {
         setTimeout(
             function()
             {
                 var updateNumOfRows = $('.spec-ticket-item.ticket-item.ember-view:not(.loading)').length;
                 $("#numberVal").text( ' (' + updateNumOfRows + ')' );
-            }, 1000);
+            }, 1500);
+    }
 
-        /*         $('#ember1824').scroll(function() {
-            if ($('.ticket-grid-container').is(':visible')) {
-                console.log("Scroll");
-            }*/
-    });
-});
-
-
-
-
-
-// This class name is for the refresh button
-/*     $('.refresh-btn.btn.btn-default.js-refresh-btn').click(function() {
-
+    //Once user scrolls to a certain point in the container, the count for some reason messes up. This functions fixes the issue.
+    $.fn.TicketCounterSubtractFive = function()
+    {
         setTimeout(
             function()
             {
-                var updateNumOfRows = $('.spec-ticket-item.ticket-item.ember-view:not(.loading)').length;
-                console.log(updateNumOfRows);
-                $("#numberVal").text( ' (' + updateNumOfRows + ')' );
-            }, 1000);
+                var recountedNumOfRows = $('.spec-ticket-item.ticket-item.ember-view:not(.loading)').length - 5;
+                $("#numberVal").text( ' (' + recountedNumOfRows + ')' );
+            }, 1500);
+    }
 
-        console.log("Button Clicked");
-    }); */
-
-
-//This class is for the inner class refresh buttion.
-/*     $('.glyphicon.glyphicon-refresh.spec-refresh-btn').click(function() {
-
-        setTimeout(
-            function()
-            {
-                var updateNumOfRows = $('.spec-ticket-item.ticket-item.ember-view:not(.loading)').length;
-                console.log(updateNumOfRows);
-                $("#numberVal").text( ' (' + updateNumOfRows + ')' );
-            }, 1000);
-
-        console.log("Button Clicked");
-    }); */
-
-
-
-//Find where container is scrolling.
-/*     $('#ember1824').scroll(function() {
-        if ($('.ticket-grid-container').is(':visible')) {
-            console.log("Scroll");
+    //This function if to fix the counter mistake once the user scrolls to a certain point
+    $('.ember-view').scroll(function(){
+        var currentScrollPosition = $(this).scrollTop();
+        if(currentScrollPosition >= 1590)
+        {
+            scroll = true;
+            $('#unassigned_tickets').click(function() { clickCheck = true; $.fn.TicketCounterSubtractFive(); });
+            $('#my_tickets').click(function() { clickCheck = true; $.fn.TicketCounterSubtractFive(); });
+            $('#waiting_tickets').click(function() { clickCheck = true; $.fn.TicketCounterSubtractFive(); });
+            $('.refresh-btn.btn.btn-default.js-refresh-btn').click(function() { if(clickCheck == true) { $.fn.TicketCounterSubtractFive(); } });
+            $('.btn.btn-default.refreshButton').click(function() { if(clickCheck == true) { $.fn.TicketCounterSubtractFive(); } });
         }
-    }); */
+    });
 
+    //When dropdown is clicked, if true, display the number. Otherwise don't display anything.
+    $('#unassigned_tickets').click(function() { clickCheck = true; $.fn.TicketCounter(); });
+    $('#my_tickets').click(function() { clickCheck = true; $.fn.TicketCounter(); });
+    $('#waiting_tickets').click(function() { clickCheck = true; $.fn.TicketCounter(); });
+    $('#open_tickets').click(function() { clickCheck = false; });
+    $('#closed_tickets').click(function() { clickCheck = false; });
+    $('#all_tickets').click(function() { clickCheck = false; });
+    $('#alerted_tickets').click(function() { clickCheck = false; });
+    $('#past_due_tickets').click(function() { clickCheck = false; });
+    $('.ticket-view-link.ember-view.sui-dropdown_entry').click(function() { $("#numberVal").text( ' ' ); });
 
-
-/*
-NOTES:
-
-The class name "spec-ticket-item ticket-item  ember-view loading" happens after all the rows have been displayed.
-
-When we run the class name "spec-ticket-item ticket-item  ember-view.length",
-it also counts the class name that says "spec-ticket-item ticket-item  ember-view.length".
-That's the reason why we are subtracting rowCount and uncountLoading.
-
-Rightclick Inspect to see details
-
-*/
+    // These two class name is for the refresh button to recount tickets when clicked in case previous function didn't load correctly or when new ticket is added
+    $('.refresh-btn.btn.btn-default.js-refresh-btn').click(function() { if(clickCheck == true) { $.fn.TicketCounter(); } });
+    $('.btn.btn-default.refreshButton').click(function() { if(clickCheck == true) { $.fn.TicketCounter(); } });
+});
